c4cf94497f33dd755c36edfc860f30ee2172f4b7
kwizeraangelus <137456662+kwizeraangelus@users.noreply.github.com>
2026-05-13 09:30:44 +0200

    updates of typeOrmMOdule in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -28,8 +28,9 @@ import { InnovationModule } from './innovation/innovation.module';
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
+      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+
+      synchronize: false,
 
       // development settings
       dropSchema: false,
